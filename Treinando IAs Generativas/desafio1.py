f9dd503da0e7e8ff03bbb7077501c14d56834571
Thiago <thiago@localhost.localdomain>
2024-05-16 19:10:58 -0300

    Desafios 1 e 3

---
+++ Treinando IAs Generativas/desafio1.py
@@ -0,0 +1,28 @@
+# Dicionário associando características aos modelos Claude 3
+caracteristicas_modelos = {
+    "automatizar tarefas": "Claude 3 Opus",
+    "pesquisa e desenvolvimento": "Claude 3 Opus",
+    "resposta rápida e capacidade de resposta quase instantânea": "Claude 3 Haiku",
+    "motores de chatbots de lojas": "Claude 3 Haiku",
+    "moderação de conteúdo": "Claude 3 Haiku",
+    "processamento de tarefas mais básicas": "Claude 3 Haiku",
+    "raciocínio cuidadoso": "Claude 3 Sonnet",
+    "processamento de dados": "Claude 3 Sonnet",
+    "aplicações de vendas": "Claude 3 Sonnet",
+    "extração de texto de imagens": "Claude 3 Sonnet",
+    "equilíbrio ideal entre inteligência e velocidade": "Claude 3 Sonnet",
+   
+}
+
+def encontrar_modelo(caracteristica_fornecida):
+  if not caracteristica_fornecida.strip():
+    return "Descrição inválida."
+
+  for elemento, valor in caracteristicas_modelos.items():
+    if elemento.lower() in caracteristica_fornecida.lower():  
+      return valor
+  return "Modelo não encontrado."
+caracteristica_fornecida = input()
+
+modelo_correspondente = encontrar_modelo(caracteristica_fornecida)
+print(modelo_correspondente)
